b99268b705a1bbcab218a516940f18cfa1843b98
Stefanos Stefanou <stefanos.stefanou@century.tech>
2022-09-09 14:44:15 +0100

    Challenge Ch3

--- src/main/scala/ch3/Task1.scala
+++ src/main/scala/ch3/Task1.scala
@@ -2,7 +2,7 @@ package ch3
 
 import scala.util.Try
 
-object Writer {
+object Task1 {
 
   // A is every object. Functions from Writer[A] => Writer[B] are morphisms
   type Writer[A] = (A, String) // basic structure to carry extra info
@@ -25,42 +25,11 @@ object Writer {
           case Some(value) =>
             val a = right(value)
             (a._1, resA._2 + "\n" + a._2)
-          case None => None.liftToWriter
+          case None => None.liftToWriter.withLog(resA._2)
         }
         resB
       }
   }
-  abstract class MyMonoid[A] {
-    def combine(a: A)(b: A): A
-    def id: A
-  }
-  class MyOptionMonoid[A](implicit aMonoid: MyMonoid[A]) extends MyMonoid[Option[A]] {
-    override def combine(a: Option[A])(b: Option[A]): Option[A] = {
-      (a, b) match {
-        case (Some(v1), Some(v2)) => Some(aMonoid.combine(v1)(v2))
-        case (Some(v1), None)     => Some(v1)
-        case (None, Some(v2))     => Some(v2)
-        case (None, None)         => None
-      }
-    }
-    override def id: Option[A] = None
-  }
-
-  abstract class MyMonad[F[_]] {
-    def map[A, B](v: F[A])(fn: A => B): F[B]
-    def flatMap[A, B](v: F[A])(fn: A => F[B]): F[B]
-    def pure[A](a: A): F[A]
-  }
-  class MyWriterMonad extends MyMonad[Writer] {
-    override def map[A, B](v: Writer[A])(fn: A => B): (B, String) = flatMap(v)(fn andThen pure)
-    override def flatMap[A, B](v: Writer[A])(fn: A => Writer[B]): Writer[B] = {
-      val logA = v._2
-      val opB  = fn(v._1)
-      (opB._1, logA + opB._2)
-    }
-    override def pure[A](a: A): Writer[A] = a.liftToWriter
-  }
-
   implicit class WriterLiftOp[A](a: A) {
     def liftToWriter: Writer[A] = (a, "")
   }
@@ -80,16 +49,14 @@ object Writer {
   }
 
   def safe_root(x: Double): Writer[Option[Double]] =
-    Try
-      .apply(Math.sqrt(x))
-      .toOption
+    Option
+      .when(x != 0d)(Math.sqrt(x))
       .liftToWriter
       .withLogIfSuccess("Safe Root")
       .withLogIfFailure("Safe Root of 0 Undefined")
   def safe_reciprocal(x: Double): Writer[Option[Double]] =
-    Try
-      .apply(1 / x)
-      .toOption
+    Option
+      .when(x != 0)(1 / x)
       .liftToWriter
       .withLogIfSuccess("Safe Reciprocal")
       .withLogIfFailure("Safe Reciprocal of 0 Undefined")
@@ -100,8 +67,8 @@ object Writer {
     val writerB: Int => Writer[Double] = _ => 42d.liftToWriter.withLog("I am 42 Double")
     // println((writerA >=> writerB)(42))
 
-    val safe_root_reciprocal: Double => Writer[Option[Double]] = safe_reciprocal _ >=> safe_root _
-    println(safe_root_reciprocal(32d))
+    val safe_root_reciprocal: Double => Writer[Option[Double]] = safe_root _ >=> safe_reciprocal _
+    println(safe_root_reciprocal(11d))
 
   }
 }
